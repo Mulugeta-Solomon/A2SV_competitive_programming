0e3eb389548f8975793191556d72499b4a8dca60
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-02-28 04:24:45 +0900

    Time: 527 ms (10.45%), Space: 18.2 MB (11.13%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -6,18 +6,32 @@ class Solution:
         for i in range(len(names)):
             name_height_look_up[heights[i]] = names[i]
         
-        for i in range(1, len(heights)):
-            curr_value = heights[i]
-            j = i - 1
+        maximum = max(heights)
+        counts = [0] * (maximum + 1)
 
-            while j >= 0 and curr_value > heights[j]:
-                heights[j+1] = heights[j]
-                j -= 1
+        for height in heights:
+            counts[height] += 1
+        
+        target = 0
+        for idx, value in enumerate(counts):
+            for i in range(value):
+                heights[target] = idx
+                target += 1
+                
+        # for i in range(1, len(heights)):
+        #     curr_value = heights[i]
+        #     j = i - 1
+
+        #     while j >= 0 and curr_value > heights[j]:
+        #         heights[j+1] = heights[j]
+        #         j -= 1
             
-            heights[j+1] = curr_value
+        #     heights[j+1] = curr_value
 
         # print(name_height_look_up)
         # print(heights)
+
+        heights = heights[::-1]
         
         result = []
         for height in heights:
