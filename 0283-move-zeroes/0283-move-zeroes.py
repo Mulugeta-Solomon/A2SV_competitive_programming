c5fa11d858957b1bbf0631fead8a4d934c0bc1dd
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-03-09 00:30:44 +0900

    Time: 115 ms (96.29%), Space: 18.1 MB (46.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,14 +6,12 @@ class Solution:
         holder, seeker = 0, 1
 
         while seeker < len(nums):
-            if nums[seeker] != 0:
+            if nums[seeker] != 0 and nums[holder] == 0:
+                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+
+            if nums[holder] != 0:
+                holder += 1
                 
-                while holder < seeker:
-                    if nums[holder] == 0:
-                        nums[holder], nums[seeker] = nums[seeker], nums[holder]
-                        break
-                    holder += 1
-            
             seeker += 1
             
            
