e6f4910d7f66b497a12938ef5349cf46abc98cac
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-03-29 00:13:48 +0900

    Time: 248 ms (12.03%), Space: 21.5 MB (17.85%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,15 +1,21 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
+         
+         '''
+         nums = [4,5,0,-2,-3,1], k = 5
+        prefix_sum = [4, 9, 9, 7, 4, 5]
         
-        remainder = [0] * k
-        remainder[0] = 1
+        remaider = [4, 4 , 4, 2, 4, 0 ]
+         '''
 
-        curr_remainder = 0
+         remainder = {0:1}
+         prefix_sum, max_subarr = 0, 0
+         for i in range(len(nums)):
+            prefix_sum += nums[i]
 
-        for num in nums:
-            curr_remainder = (num + curr_remainder) % k
-            remainder[curr_remainder] += 1
-        
-        result = sum([n * (n-1) // 2 for n in remainder])
-        
-        return result
+            if prefix_sum % k in remainder:
+                max_subarr += remainder[prefix_sum % k]
+
+            remainder[prefix_sum % k] = remainder.get(prefix_sum % k, 0) + 1
+         print(remainder)
+         return max_subarr
