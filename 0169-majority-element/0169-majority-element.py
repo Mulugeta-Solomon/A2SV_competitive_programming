d7189e7d04dc8a715140246e08b5365402e661c3
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-03-31 23:17:04 +0900

    Time: 159 ms (49.15%), Space: 18 MB (35.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        lookup, majority_element = {}, 0
+        nums.sort()
 
-        for num in nums:
-            lookup[num] = lookup.get(num, 0) + 1
-        
-        for num, freq in lookup.items():
-            if freq > len(nums)//2:
-                majority_element = num
+        majority_element = nums[len(nums)//2]
         
         return majority_element
             
