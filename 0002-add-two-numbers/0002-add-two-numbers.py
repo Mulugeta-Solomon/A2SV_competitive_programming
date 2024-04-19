1001a0681333d17faf2b17da8e74f105d9d339ee
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-04-19 23:26:29 +0900

    Time: 56 ms (50.41%), Space: 16.7 MB (9.92%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,32 +5,36 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num_1, num_2 = '', ''
+        
+        dummy = ListNode(0)
+        result = dummy 
 
-        head_1, head_2 = l1, l2
+        carry, curr_sum = 0, 0
 
-        while head_1:
-            num_1 += str(head_1.val)
-            head_1 = head_1.next
-        
-        
-        while head_2:
-            num_2 += str(head_2.val)
-            head_2 = head_2.next
-        
-        num_1 = num_1[::-1]
-        num_2 = num_2[::-1]
-        total = str(int(num_1) + int(num_2))[::-1]
-        # print(total)
-  
-        head = ListNode(int(total[0]))
-        temp = head
-        for i in range(1,len(total)):
-            head.next = ListNode(int(total[i]))
-            head = head.next
-        
-        return temp
+        while l1 or l2:
+            if l1 and l2:
+                curr_sum = l1.val + l2.val + carry
+                dummy.next = ListNode(curr_sum % 10)
+                carry = curr_sum // 10
+                l1, l2 = l1.next, l2.next
+            else:
+                if not l1:
+                    curr_sum = l2.val + carry 
+                    dummy.next = ListNode(curr_sum % 10)
+                    carry = curr_sum // 10
+                    l2 = l2.next
+                else:
+                    curr_sum = l1.val + carry 
+                    dummy.next = ListNode(curr_sum % 10)
+                    carry = curr_sum // 10
+                    l1 = l1.next
+            
+            dummy = dummy.next 
 
+        if carry > 0:
+            dummy.next = ListNode(carry)
+            dummy = dummy.next
+        
+        dummy.next = None 
 
-        
-        
+        return result.next
