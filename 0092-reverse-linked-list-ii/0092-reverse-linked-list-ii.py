bda9ac3aa82e15748362db52e2b0ef5cde6218a9
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-04-22 13:36:02 +0900

    Time: 30 ms (88.17%), Space: 16.5 MB (95.6%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        
+        if not head or not head.next:
+            return head
+
+        result = ListNode(0, head) # create dummy node 
+
+        left_prev, curr = result, head 
+        for _ in range(left - 1):
+            left_prev, curr = curr, curr.next
+        
+        prev, curr = None, curr 
+        for _ in range(right - left + 1):
+            next_node = curr.next
+            curr.next = prev
+            prev = curr 
+            curr = next_node 
+        
+        left_prev.next.next = curr 
+        left_prev.next = prev 
+        
+        return result.next
+
+
+
+
