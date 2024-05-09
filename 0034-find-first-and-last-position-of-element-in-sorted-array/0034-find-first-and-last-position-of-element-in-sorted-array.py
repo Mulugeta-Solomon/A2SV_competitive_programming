4178820613d5406450b3c2fbf0d3e297849554d6
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-05-10 00:06:03 +0900

    Time: 77 ms (49.18%), Space: 17.8 MB (84.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,40 +1,46 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        start_index = self.firstOccurence(nums,len(nums),target)
 
-        end_index = self.lastOccurence(nums, 0, len(nums) - 1,target, len(nums))
-        return [start_index, end_index]
+        left, right = 0, len(nums) - 1
 
+        starting_pos = self.startingPosition(nums, left, right, target) 
+        ending_pos = self.endingPosition(nums, left, right, target)
 
-    def firstOccurence(self,arr, n, x): 
-        start = 0 
-        end = n-1
+        return [starting_pos, ending_pos]
+    
+    def startingPosition(self, nums: List[int], left: int, right: int, target: int) -> int:
 
-        while(start <= end):
-            mid=(start + end) // 2
-            if(arr[mid] < x):
-                start = mid + 1 
-            elif(arr[mid] > x):
-                end = mid-1 
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                if mid == 0 or arr[mid-1] != arr[mid]:
+                if mid == 0 or nums[mid-1] != nums[mid]:
                     return mid 
                 else:
-                    end = mid - 1
+                    right = mid - 1
         
         return -1
-    
-    def lastOccurence(self, arr, low, high, x, n):
-        if (high >= low):
-            mid = low + (high - low) // 2
-            if ((mid == n - 1 or x < arr[mid + 1]) and arr[mid] == x):
-                return mid
-            elif (x < arr[mid]):
-                return self.lastOccurence(arr, low, (mid - 1), x, n)
-            else:
-                return self.lastOccurence(arr, (mid + 1), high, x, n)
-    
-        return -1
-        
-        
 
+    
+    def endingPosition(self, nums: List[int], left: int, right: int, target: int) -> int:
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                if mid == len(nums) - 1 or nums[mid + 1] != nums[mid]:
+                    return mid 
+                else:
+                    left = mid + 1
+        
+        return -1
+
+        
