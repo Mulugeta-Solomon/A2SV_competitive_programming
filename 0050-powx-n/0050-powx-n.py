d73d12242f1343264d919c3c43fd09c76766f60d
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-05-29 22:49:28 +0900

    Time: 42 ms (12.51%), Space: 16.6 MB (22.37%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -2,11 +2,11 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         flag = True if n < 0 else False 
         n = abs(n)
-        
+
         if n <= 0:
             return 1
         
-        if n%2 == 0:
+        if n%2 == 0: # If n is even i.e n = 4 (x**4 = (x**2)**2)
             x = x * x
             n = n // 2
          
