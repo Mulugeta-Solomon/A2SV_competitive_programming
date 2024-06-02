f010f58ff2abf2d9192bb47184a61959a795d180
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-06-03 01:17:05 +0900

    Time: 23 ms (98.42%), Space: 16.3 MB (96.3%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+                
+        result = []
+        look_up = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+        i, curr = 0, ''
+        while i < len(s):
+            if s[i] in look_up:
+                result.append([int(s[i]), curr])
+                curr = ''
+            elif s[i] == ' ':
+                i += 1
+                continue
+            else:
+                curr += s[i]
+            i += 1
+        
+        result.sort()
+        ans = ''
+        for i in range(len(result)):
+            if i == 0:
+                ans += result [i][1]
+            else:
+                ans += ' ' + result[i][1]
+        
+        return ans
+
