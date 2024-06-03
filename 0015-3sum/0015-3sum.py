14511b609af7ba1de2ddf9d493c5a6e89ab989d7
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-06-04 00:19:07 +0900

    Time: 546 ms (90.69%), Space: 20.7 MB (70.51%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,41 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
+        
         nums.sort()
         result = []
-
+        
         for i in range(len(nums) - 2):
             if nums[i] > 0:
                 break
             
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+            if i > 0 and nums[i] == nums[i-1]:
+                continue 
             
-            left, right = i + 1, len(nums) - 1 
+            left, right = i + 1, len(nums) - 1
 
             while left < right:
-                curr_sum = nums[i] + nums[left] + nums[right]
+                curr = nums[i] + nums[left] + nums[right]
 
-                if curr_sum > 0:
+                if curr > 0:
                     right -= 1
-                elif curr_sum < 0:
+                elif curr < 0:
                     left += 1
                 else:
                     result.append([nums[i], nums[left], nums[right]])
-                    last_left_occurence, last_right_occurence = nums[left], nums[right]
-                    
-                    while left < right and nums[left] == last_left_occurence:
+                    last_left_occ, last_right_occ = nums[left], nums[right]
+
+                    while left < right and nums[left] == last_left_occ:
                         left += 1
-
-                    while left < right and nums[right] == last_right_occurence:
-                        right -= 1 
-        
-        return result 
-               
-
-
-                
-
-
-
-        
+                    
+                    while left < right and nums[right] == last_right_occ:
+                        right -= 1
+        return result
