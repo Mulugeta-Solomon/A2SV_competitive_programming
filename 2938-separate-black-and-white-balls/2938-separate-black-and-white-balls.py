ad441b1c74c5d5edf84298e138c182034be8bab3
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-06-07 01:09:24 +0900

    Time: 99 ms (59.77%), Space: 17.6 MB (56.39%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -1,18 +1,15 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-
-        left, right, count = 0, 1, 0
-        balls = [int(s[i]) for i in range(len(s))]
-
-        while right <= len(balls) - 1:
-
-            if balls[right] == 0 and balls[left] == 1:
-                balls[left], balls[right] = balls[right], balls[left]
+        
+        count, result = 0, 0
+        
+        for i in range(len(s)):
+            if s[i] == '1':
                 count += 1
 
-            left += 1
-            right += 1
+            else:
+                result += count 
         
-        return count
+        return result
 
         
