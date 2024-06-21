afe5a22f3968fbf72274b681b563adc2795c4f35
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-06-21 23:30:28 +0900

    Time: 1069 ms (40.4%), Space: 55.9 MB (22.73%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,29 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        
+        graph = {1: ((0, -1), (0, 1)), 
+                 2: ((-1, 0), (1, 0)), 
+                 3: ((0, -1), (1, 0)),
+                 4: ((0, 1), (1, 0)), 
+                 5: ((0, -1), (-1, 0)),
+                 6: ((0, 1), (-1, 0)) }
+
+        def isInbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        def dfs(row, col):
+            # Base case
+            if row == len(grid) - 1 and col == len(grid[0]) - 1:
+                return True
+
+            visited.add((row, col))
+            for r, c in graph[grid[row][col]]:
+                newRow, newCol = row + r, col + c
+
+                if isInbound(newRow, newCol) and (newRow, newCol) not in visited and (-r, -c) in graph[grid[newRow][newCol]]:
+                    if dfs(newRow, newCol):
+                        return True
+            
+            return False
+        visited = set()
+        return dfs(0, 0)
