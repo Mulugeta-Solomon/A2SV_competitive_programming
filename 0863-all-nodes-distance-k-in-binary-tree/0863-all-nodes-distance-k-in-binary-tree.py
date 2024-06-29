7706c139b1db0398868a91a18fa56d0766e41497
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-06-30 00:49:55 +0900

    Time: 45 ms (18.72%), Space: 16.9 MB (49.86%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -8,37 +8,38 @@
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         
-        queue = deque([(root, 0, 0)])
+        def bfs(root, parent):
+            if not root:
+                return None 
+            parent_dict[root.val] = parent
+
+            left = bfs(root.left, root)
+            right = bfs(root.right, root) 
+
+        parent_dict = defaultdict()
+        bfs(root, None)
+        
         result = []
-        visited = [0, 1]
-        visited_sofar = defaultdict(list)
-        
+        queue = deque([(target, 0)])
+        visited = set()
+
         while queue:
-            size = len(queue)
-            
-            for _ in range(size):
-                node, level, idx = queue.popleft()
+            node, level = queue.popleft()
+
+            if node.val not in visited:
+                visited.add(node.val)
+
+                if level == k:
+                    result.append(node.val)
                 
-                if node.val == target.val:
-                    visited = [level, idx]
-                else:
-                    visited_sofar[node.val] = [level, idx]
+                if level <= k:
+                    if node.left:
+                        queue.append((node.left, level + 1))
+                    if node.right:
+                        queue.append((node.right, level + 1))
+                    
+                    if parent_dict[node.val]:
+                        queue.append((parent_dict[node.val], level + 1))
 
-                if node.left:
-                    queue.append((node.left, level + 1, idx * 2))
-                if node.right:
-                    queue.append((node.right, level + 1, idx * 2 + 1))
-    
-
-        for key in visited_sofar.keys():
-            curr_level, curr_idx = visited_sofar[key]
-            if curr_level != visited[0]:
-                if abs(curr_level - visited[0]) == k:
-                    result.append(key)
-            else:
-                if abs(curr_idx - visited[1]) + 1 == k:
-                    result.append(key)
-
-        
         return result 
                 
