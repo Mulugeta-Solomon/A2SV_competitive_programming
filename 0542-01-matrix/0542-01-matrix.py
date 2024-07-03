20fc88a48307680c97a2a21c2c30bd10daa0fe99
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-07-04 00:15:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        queue = deque()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        rows, cols = len(mat), len(mat[0])
+
+        def isInbound(x, y):
+            return 0 <= x < rows and 0 <= y < cols
+
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+                else:
+                    mat[r][c] = -1 # not checked yet
+        
+        result = [[0] * cols for _ in range(rows)]
+
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                r, c = queue.popleft()
+
+                for direction in directions:
+                    newRow, newCol = r + direction[0], c + direction[1]
+
+                    if isInbound(newRow, newCol) and mat[newRow][newCol] == -1:
+                        queue.append((newRow, newCol))
+                        mat[newRow][newCol] = 1 # mark visited
+                        result[newRow][newCol] = mat[r][c] + 1
+        
+        return result
