9a55c4dd78e32d378d633e48438f8e434fc04991
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-07-17 00:49:17 +0900

    Time: 47 ms (24.27%), Space: 16.7 MB (6.19%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -3,18 +3,25 @@ class Solution:
 
         last_appeard = {s[i]:i for i in range(len(s))}
         
-        first, result = 0, []
+        first, end, result = 0, 0, []
+        
+        for i, _  in enumerate(s):
+            end = max(end, last_appeard[s[i]])
 
-        while first < len(s):
-            end, second = last_appeard[s[first]], first + 1
+            if i == end:
+                result.append(end - first + 1)
+                first = end + 1
 
-            while second < end:
-                if last_appeard[s[second]] > end:
-                    end = last_appeard[s[second]]
-                second += 1
+        # while first < len(s):
+        #     end, second = last_appeard[s[first]], first + 1
+
+        #     while second < end:
+        #         if last_appeard[s[second]] > end:
+        #             end = last_appeard[s[second]]
+        #         second += 1
             
-            result.append(end - first + 1)
-            first = end + 1
+        #     result.append(end - first + 1)
+        #     first = end + 1
 
         return result 
 
