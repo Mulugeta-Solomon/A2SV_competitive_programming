f7a36fab92138b9fe8486a1f3b9f6e1faa2dcf83
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-08-03 01:31:51 +0900

    Time: 363 ms (65.81%), Space: 26 MB (58.12%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -0,0 +1,31 @@
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        
+        graph = defaultdict(list)
+        indegree = [0] * len(quiet)
+
+        for src, dst in richer:
+            graph[src].append(dst)
+            indegree[dst] += 1
+        queue, result = deque(), [i for i in range(len(quiet))]
+
+        for i in range(len(quiet)):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                for neighbor in graph[node]:
+                    if quiet[result[neighbor]] > quiet[result[node]]:
+                        result[neighbor] = result[node]
+                    
+                    # print(node, neighbor)
+                    # print(result)
+                    
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        queue.append(neighbor)
+        return result
+        
