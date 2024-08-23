46595997320912029db2f317d017cda8904678ae
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-08-23 22:43:18 +0900

    Time: 41 ms (20.95%), Space: 16.4 MB (94.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,22 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0 or nums is None:
+            return 0
+
+        if len(nums) <= 2:
+            return max(nums)
         
-        n = len(nums)
-        if n == 1: return nums[0]
 
-        memo1 = [0] * (n)
-        memo1[0] = nums[0]
-        memo1[1] = max(nums[0], nums[1])
+        return max(self.rob_simple(nums[:-1]), self.rob_simple(nums[1:]))
 
-        for i in range(2, n - 1):
-            memo1[i] = max(memo1[i - 1], memo1[i - 2] + nums[i])
+    def rob_simple(self, nums: List[int]) -> int:
+        t1 = nums[0]
+        t2 = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            temp = t2
+            t2 = max(nums[i] + t1, t2)
+            t1 = temp
 
-        memo2 = [0] * (n)
-        memo2[-1] = nums[-1]
-        memo2[-2] = max(nums[-1], nums[-2])
-
-        for i in range(n - 3, 0, -1):
-            memo2[i] = max(memo2[i + 1], memo2[i + 2] + nums[i])
-    
-
-        return max(memo1[-2], memo2[1])
+        return t2
