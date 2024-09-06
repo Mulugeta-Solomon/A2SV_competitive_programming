609ef8ebc5931289b085cbbfae32def2b4ae9d20
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-09-06 22:50:26 +0900

    Time: 60 ms (32.21%), Space: 17.1 MB (61.37%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,49 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.size = [1] * size
+
+    # def find(self, x):
+    #     if x == self.root[x]:
+    #         return x
+    #     self.root[x] = self.find(self.root[x])
+    #     return self.root[x]
+
+    def find(self, x): # Iterative way of path compression 
+        while x != self.root[x]:
+            self.root[x] = self.root[self.root[x]]
+            x = self.root[x]
+        return x
+       
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.size[rootX] > self.size[rootY]:
+                self.root[rootY] = rootX
+                self.size[rootX] += self.size[rootY]
+            else:
+                self.root[rootX] = rootY
+                self.size[rootY] += self.size[rootX]
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = 0
+
+        for u, v in edges:
+            n = max(n, u, v)
+        
+        graph = UnionFind(n + 1)
+        i, idx = 0, []
+        while i < len(edges):
+            u, v = edges[i]
+            if graph.find(u) != graph.find(v):
+                graph.union(u, v)
+            else:
+                idx.append(i)
+
+            i += 1
+
+        return edges[idx[-1]] 
+
+        
