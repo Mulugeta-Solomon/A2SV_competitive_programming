b0cc2f926cd273512a892d2fa6df62598076701b
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-10-04 23:42:27 +0900

    Time: 879 ms (5%), Space: 16.7 MB (22.26%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,21 +1,38 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+
+        def dfs(copy, paste):
+            if copy == n:
+                return 0
+            if copy > n:
+                return float('inf')
+            
+            opr1 = 2 + dfs(copy * 2, copy) # copy and paste
+            opr2 = 1 + dfs(copy + paste, paste) # paste
+            
+            return min(opr1, opr2)
         
-        opr, prev, copy = 0, 1, 0
+        return 1 + dfs(1, 1)
 
-        while True:
-            if 2 * prev > n: # paste operation
-                if prev >= n:
-                    return opr
-                prev += copy
-                opr += 1
 
-            elif 2 * prev <= n: # copy and paste operation
-                copy = prev
-                prev += copy
-                opr += 2
-            else:
-                return opr
+
+        
+        # opr, prev, copy = 0, 1, 0
+
+        # while True:
+        #     if 2 * prev > n: # paste operation
+        #         if prev >= n:
+        #             return opr
+        #         prev += copy
+        #         opr += 1
+
+        #     elif 2 * prev <= n: # copy and paste operation
+        #         copy = prev
+        #         prev += copy
+        #         opr += 2
+
 
                 
 
