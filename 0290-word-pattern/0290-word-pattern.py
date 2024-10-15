28b23e4b9486274f1949351478f19308c8646ae7
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-10-15 23:05:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+
+        if len(set(s.split(' '))) != len(set(pattern)):
+            return False
+
+        lookUp = defaultdict(str)
+        s = [char for char in s.split(' ')]
+
+        for i, char in enumerate(pattern):
+            if char not in lookUp:
+                lookUp[char] = s[i]
+                continue
+            if lookUp[char] != s[i]:
+                return False
+        
+        return True
+
+
+
+
+        
