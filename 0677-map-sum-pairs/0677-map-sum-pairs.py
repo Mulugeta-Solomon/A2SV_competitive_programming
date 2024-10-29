4db2f2a7d9091383f86e20e6da65d2453416b675
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-10-30 00:18:48 +0900

    Time: 0 ms (100%), Space: 16.9 MB (6.68%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.py
+++ 0677-map-sum-pairs/0677-map-sum-pairs.py
@@ -1,32 +1,49 @@
-# class TrieNode:
-#     def __init__(self, ):
-#         self.children = {}
-#         self.is_last = False
+class TrieNode:
+    def __init__(self, val):
+        self.children = [None for _ in range(26)]
+        self.val = val
+
+class Trie:
+    def __init__(self):
+        self.dict = TrieNode(0)
+    
+    def insert(self, word: str, val: int) -> None:
+        curr = self.dict
+
+        for char in word:
+            idx = ord(char) - ord('a')
+            if not curr.children[idx]:
+                curr.children[idx] = TrieNode(0)
+            curr = curr.children[idx]
+            curr.val += val
+    
+    def search(self, prefix: str) -> int:
+        curr = self.dict
+        
+        for char in prefix:
+            idx = ord(char) - ord('a')
+            if not curr.children[idx]:
+                return 0
+            
+            curr = curr.children[idx]
+    
+        return curr.val
+
 
 class MapSum:
 
     def __init__(self):
-        self.trie = {}
+        self.trie = Trie()
+        self.existed = defaultdict(int)
+
 
     def insert(self, key: str, val: int) -> None:
-        curr = self.trie
-        curr[key] = val
-
+        currVal = val - self.existed[key]
+        self.existed[key] = val
+        self.trie.insert(key, currVal)
 
     def sum(self, prefix: str) -> int:
-        result = 0
-        for key in self.trie:
-            flag = True
-            if len(key) < len(prefix):
-                continue
-            for i, char in enumerate(prefix):
-                if char != key[i]:
-                    flag = False
-                    break
-            if flag:
-                result += self.trie[key]
-        
-        return result 
+        return self.trie.search(prefix)
 
 
 
