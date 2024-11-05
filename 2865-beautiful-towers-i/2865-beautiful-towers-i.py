d52e659eceaa088271672e394abc4dd590e44b3a
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-11-06 01:01:00 +0900

    Time: 1414 ms (35.3%), Space: 16.8 MB (73.17%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.py
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.py
@@ -1,22 +1,21 @@
 class Solution:
     def maximumSumOfHeights(self, heights: List[int]) -> int:
-        peakIdx, tower = None, float('-inf')
+        max_sum = float('-inf')
 
-        for i, height in enumerate(heights):
-            if tower <= height:
-                tower = height 
-                peakIdx = i
-        
-        result = heights[peakIdx]
-        curr = heights[peakIdx]
-        for i in range(peakIdx + 1, len(heights)):
-            curr = min(curr, heights[i])
-            result += curr
-        
-        curr = heights[peakIdx]
+        for peakIdx in range(len(heights)):
+            result = heights[peakIdx]
+            curr = heights[peakIdx]
+            
+            for i in range(peakIdx + 1, len(heights)):
+                curr = min(curr, heights[i])
+                result += curr
+            
+            curr = heights[peakIdx]
 
-        for i in range(peakIdx - 1, -1, -1):
-            curr = min(curr, heights[i])
-            result += curr
+            for i in range(peakIdx - 1, -1, -1):
+                curr = min(curr, heights[i])
+                result += curr
+            
+            max_sum = max(max_sum, result)
         
-        return result
+        return max_sum
