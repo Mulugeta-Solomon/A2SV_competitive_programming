cace741fad3558f8eaba0ba1a9287bb62b0d22e7
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-11-11 14:30:22 +0900

    Time: 158 ms (18.99%), Space: 16.9 MB (12.98%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -0,0 +1,39 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        
+        def prime(n):
+            if n <= 2:
+                return 0
+            prime = [1] * n 
+            prime[0] = prime[1] = 0
+            i = 2
+            while i * i <= n - 1:
+                if prime[i] == 1:
+                    j = i*i
+                    while j < n:
+                        prime[j] = 0
+                        j += i
+                i += 1
+
+            for i in range(n - 1, -1, -1):
+                if prime[i]:
+                    return i
+
+        
+        for i in range(len(nums) - 1):                        
+            if i > 0:
+                n = nums[i] - nums[i - 1]
+            else:
+                n = nums[i]
+            
+            curr = prime(n)
+            nums[i] -= curr
+
+            if nums[i] >= nums[i + 1]:
+                return False
+            if i > 0 and nums[i] <= nums[i - 1]:
+                return False
+
+        return True
+
+        
