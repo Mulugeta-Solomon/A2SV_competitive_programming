97deb4830824e81b38abb39d187ae4a6152ca0f1
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-11-12 15:00:53 +0900

    Time: 398 ms (6.1%), Space: 63.1 MB (91.94%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -1,42 +1,29 @@
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        lookUp, result = defaultdict(int), [0] * len(queries)
+        items.sort()
+        curr_max, result = 0, [0] * len(queries)
 
-        for price, beauty in items:
-            if lookUp[price] < beauty:
-                lookUp[price] = beauty
-        
-        prices = list(sorted(map(int, lookUp.keys())))
-        print(lookUp)
-        def binarySearch(target, prices):
-            left, right = 0, len(prices) - 1
+        for i in range (len(items)):
+            curr_max = max(curr_max, items[i][1])
+            items[i][1] = curr_max
 
-            while left < right:
+        def binarySearch(items, target):
+            left, right, result = 0, len(items) - 1, 0
+
+            while left <= right:
                 mid = left + (right - left) // 2
 
-                if target > prices[mid]:
-                    left =  mid + 1 
+                if items[mid][0] > target:
+                    right =  mid - 1 
                 else:
-                    right = mid 
-            
-            return left
-        print(prices)
+                    result = max(result, items[mid][1])
+                    left = mid + 1
+    
+            return result
+
         for i in range(len(queries)):
-            idx = binarySearch(queries[i], prices)
-            # idx = bisect_right(prices, queries[i]) - 1
-            # print(idx, queries[i])
-            print(i, idx)
-            if idx < 0:
-                if queries[i] >= prices[0]:
-                    result[i] = lookUp[prices[0]]
-            elif idx > len(prices):
-                if queries[i] >= prices[-1]:
-                    result[i] = lookUp[prices[-1]]
-            else:
-                if queries[i] >= prices[idx]:
-                    result[i] = lookUp[prices[idx]]
-                else:
-                    if idx > 0:
-                        result[i] = lookUp[prices[idx - 1]]
+            curr = binarySearch(items, queries[i])
+            if curr > 0:
+                result[i] = curr
 
         return result
