80b90e712d2bef568d6bf56ac9a985c8e1d4e34e
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-12-01 05:00:00 +0900

    Time: 311 ms (80.54%), Space: 89.8 MB (56.46%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.py
@@ -1,33 +1,27 @@
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        graph  = defaultdict(deque)
+        graph  = defaultdict(list)
         indegree, outdegree = defaultdict(int), defaultdict(int)
-        result, startNode = [], -1
+        result = []
 
         for start, end in pairs:
             graph[start].append(end)
             outdegree[start] += 1
             indegree[end] += 1
                 
-        def dfs(node):
-            while graph[node]:
-                next_node = graph[node].popleft()
+        def dfs(curr):
+            while graph[curr]:
+                next_node = graph[curr].pop()
                 dfs(next_node)
-            
-            result.append(node)
+                result.append([curr, next_node])
         
+        startNode = pairs[0][0]
         for node in outdegree:
             if outdegree[node] == indegree[node] + 1:
                 startNode = node
                 break
         
-        if startNode == -1:
-            startNode = pairs[0][0]
-        
         dfs(startNode)
-        result = result[::-1]
 
-        return [[result[i-1], result[i]] for i in range(1, len(result))]
-
-
-        
+        return result [::-1]
+     
