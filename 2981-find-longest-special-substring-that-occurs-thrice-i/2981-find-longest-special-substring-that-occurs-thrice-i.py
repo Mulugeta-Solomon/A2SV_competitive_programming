ee82f4cf95286ce538dc97a5467e907a212dff9c
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2024-12-10 22:47:57 +0900

    Time: 41 ms (20%), Space: 17.2 MB (13.91%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        
+        def is_possible(n):
+            lookUp, left = defaultdict(int), 0
+
+            while left + n <= len(s) :
+                lookUp[s[left:left + n]] += 1
+                left += 1
+
+            for key, val in lookUp.items():
+                if len(set(key)) == 1 and val >= 3:
+                    return True
+            
+            return False 
+
+        left, right, result = 1, len(s) - 1, -1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if is_possible(mid):
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result  
