16b48d1acd67fa77dd869b44015792ee3717aad9
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-01-22 15:08:19 +0900

    Time: 2397 ms (10.12%), Space: 81.6 MB (78.82%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.py
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.py
@@ -7,13 +7,14 @@ class Solution:
         
         def isInbound(x, y):
             return 0 <= x < rows and 0 <= y < cols
-
+        heights = [[-1 for _ in range(cols)] for _ in range(rows)]
         for r in range(rows):
             for c in range(cols):
                 if isWater[r][c] == 1:
                     queue.append((r, c, 0))
+                    heights[r][c] = 0
         
-        heights = [[-1 for _ in range(cols)] for _ in range(rows)]
+        
 
         while queue:
             size = len(queue)
@@ -21,12 +22,13 @@ class Solution:
             for _ in range(size):
                 r, c, curr = queue.popleft()
 
-                if heights[r][c] == -1:
-                    heights[r][c] = curr
+                # if heights[r][c] == -1:
+                #     heights[r][c] = curr # mark visited
        
-                    for direction in directions:
-                        newRow, newCol = r + direction[0], c + direction[1]
-                        if isInbound(newRow, newCol) and  heights[newRow][newCol] == -1:
-                            queue.append((newRow, newCol, curr + 1))
+                for direction in directions:
+                    newRow, newCol = r + direction[0], c + direction[1]
+                    if isInbound(newRow, newCol) and  heights[newRow][newCol] == -1:
+                        queue.append((newRow, newCol, curr + 1))
+                        heights[newRow][newCol] = curr + 1
 
         return heights
