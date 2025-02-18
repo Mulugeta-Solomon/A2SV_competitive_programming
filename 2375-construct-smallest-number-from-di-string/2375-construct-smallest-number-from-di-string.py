15a65834f2ad9c0ebede29baada58c7cc31370cf
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-02-18 15:44:04 +0900

    Time: 2 ms (26.5%), Space: 17.7 MB (83.23%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        result, stack = [], [] # stack
+
+        for i in range(len(pattern) + 1):
+            stack.append(str(i + 1))
+            if i == len(pattern) or pattern[i] == 'I':
+                while stack:
+                    result.append(stack.pop())
+        
+        return ''.join(result)
+
+
+        
