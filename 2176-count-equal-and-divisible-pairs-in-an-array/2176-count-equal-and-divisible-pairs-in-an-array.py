a6cbc1cf212f149dd423c7f573efaed7294ba7a4
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-02-22 02:37:05 +0900

    Time: 20 ms (65.46%), Space: 17.9 MB (47.72%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -4,7 +4,7 @@ class Solution:
 
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j] and not ((i * j) % k):
+                if nums[i] == nums[j] and not i * j % k:
                     result += 1
         
         return result
