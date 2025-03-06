a4a02fc19976d38ad8c7ae6f1755c3d994c45705
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-03-06 23:57:34 +0900

    Time: 343 ms (20.68%), Space: 19.2 MB (8.46%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,43 +1,17 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        hei_name = dict(zip(heights, names))
+        max_ele = max(heights)
 
-        name_height_look_up = {}
-
-        for i in range(len(names)):
-            name_height_look_up[heights[i]] = names[i]
-        
-        maximum = max(heights)
-        counts = [0] * (maximum + 1)
+        count = [0] * (max_ele + 1)
 
         for height in heights:
-            counts[height] += 1
-        
-        target = 0
-        for idx, value in enumerate(counts):
-            for i in range(value):
-                heights[target] = idx
-                target += 1
-                
-        # for i in range(1, len(heights)):
-        #     curr_value = heights[i]
-        #     j = i - 1
+            count[height] += 1
 
-        #     while j >= 0 and curr_value > heights[j]:
-        #         heights[j+1] = heights[j]
-        #         j -= 1
-            
-        #     heights[j+1] = curr_value
+        idx = 0
+        for i in range(len(count) - 1, -1, -1):
+            for _ in range(count[i]):
+                heights[idx] = i
+                idx += 1
 
-        # print(name_height_look_up)
-        # print(heights)
-
-        heights = heights[::-1]
-        
-        result = []
-        for height in heights:
-            result.append(name_height_look_up[height])
-        
-        return result
-
-
-        
+        return [hei_name[height] for height in heights]
