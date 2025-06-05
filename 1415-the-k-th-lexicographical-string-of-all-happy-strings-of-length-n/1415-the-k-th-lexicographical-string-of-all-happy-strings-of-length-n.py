df2e7d7bf928989dd31de40e5269f9a1ee031134
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-06-05 22:59:43 +0900

    Time: 77 ms (10.17%), Space: 20 MB (30.89%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -5,18 +5,20 @@ class Solution:
             if len(curr) == n:
                 result.append(curr[:])
                 return 
-
+            
             for j in range(3):
                 if i != j:
-                    curr.append(chars[j])
+                    curr.append(choices[j])
                     backtrack(curr, j)
                     curr.pop()
 
-        result = []
-        chars = ['a', 'b', 'c']
+
+        choices, result = ['a', 'b', 'c'], []
         for i in range(3):
-            curr = [chars[i]]
+            curr = [choices[i]]
             backtrack(curr, i)
         
-        
-        return '' if len(result) < k else ''.join(result[k - 1])
+
+        if len(result) < k:
+            return ''
+        return ''.join(result[k - 1])
