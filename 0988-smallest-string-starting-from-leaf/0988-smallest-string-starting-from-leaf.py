1284a66a4f0edb3310ce44511b0f3e76d39d554d
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2025-06-26 23:14:14 +0900

    Time: 6 ms (50.3%), Space: 19.3 MB (12.5%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -7,29 +7,28 @@
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
         
-        def dfs(node, curr):
-            curr.append(chr(node.val + 97))
-            
-            if not node.right and not node.left:
-                # result.append(''.join(curr[:]))
+        def dfs(node, path):
+            path.append(chr(node.val + 97))
+            # base case
+            if not node.left and not node.right: # if leaf node
                 if not result:
-                    result. append(''.join(curr[::-1]))
+                    result.append(''.join(path[::-1]))
                 else:
-                    result[0] = min(result[0], ''.join(curr[::-1]))
-                curr.pop()
+                    result[0] = min(result[0], ''.join(path[::-1]))
                 return 
-        
+            
             if node.left:
-                dfs(node.left, curr)
+                dfs(node.left, path)
+                path.pop()
             if node.right:
-                dfs(node.right, curr)
-            curr.pop()
-
+                dfs(node.right, path)
+                path.pop()
         
         result = []
         dfs(root, [])
         
-        # result = [s[::-1] for s in result]
-        # result.sort()
-        
         return result[0]
+
+
+
+
