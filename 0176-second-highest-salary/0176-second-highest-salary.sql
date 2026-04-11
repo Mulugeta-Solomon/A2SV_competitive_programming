7dae96b6eb47824b2a1bcb8f6b72d33b9a6e9a2a
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2026-04-11 15:51:19 +0800

    Time: 388 ms (40.58%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+
+SELECT  MAX(salary) AS SecondHighestSalary
+FROM Employee 
+
+WHERE salary < (    SELECT MAX(salary) 
+                    FROM Employee)
