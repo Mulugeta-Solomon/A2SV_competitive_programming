b121c9796cbcd7a99ce429c6732d2c55e6229d3e
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2026-04-23 23:57:44 +0800

    Time: 669 ms (33.41%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below */
+
+SELECT Employee.name
+FROM (
+    SELECT 
+        e1.id,
+        COUNT(e1.id) AS count
+
+    FROM Employee e1
+    INNER JOIN Employee e2
+    ON e1.id = e2.managerId
+    GROUP BY e1.id
+)t, Employee
+
+WHERE t.id =　Employee.id AND t.count >= 5
