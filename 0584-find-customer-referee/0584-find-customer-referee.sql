f1e6d2a5bc673f33c9d7d56b7a3db6ea314bcb1a
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2026-04-23 16:16:19 +0800

    Time: 469 ms (68.71%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,14 +1,7 @@
 /* Write your T-SQL query statement below */
 
-WITH lookup AS (
-    SELECT 
-        name, 
-        COALESCE(referee_id, 1) AS id
-    
-    FROM Customer
-) 
-
-
 SELECT name 
-FROM lookup
-WHERE id != 2
+
+FROM Customer
+
+WHERE COALESCE(referee_id, 1) != 2
