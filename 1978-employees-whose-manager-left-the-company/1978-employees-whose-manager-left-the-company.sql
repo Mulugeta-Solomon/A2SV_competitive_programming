5bfa0531989b15f40a0cd2a30a661b03b64cd69f
Mulugeta Solomon <92956756+Mulugeta-Solomon@users.noreply.github.com>
2026-04-24 13:11:11 +0800

    Time: 537 ms (15.91%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+SELECT 
+    employee_id
+
+FROM Employees 
+
+WHERE manager_id NOT IN (SELECT employee_id FROM Employees)
+     AND salary < 30000 
+
+ORDER BY employee_id
